ab5ba13becd18dee1a9659aef6352e375b6a7013
Johnny <evgkisevgkis@gmail.com>
2023-07-02 18:20:47 +0500

    Написание тестов к классу Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item('a', 100, 2)
+    assert item1.calculate_total_price() == 200
+
+
+def test_apply_discount():
+    item1 = Item('a', 100, 2)
+    item1.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 50
