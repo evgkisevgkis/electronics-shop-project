13a78b152841d791369c5c4f10c735e67283d3dc
Johnny <evgkisevgkis@gmail.com>
2023-07-09 17:10:07 +0500

    Необходимые для корректного запуска исправления

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,7 +2,6 @@ from src.item import Item
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
-
     # длина наименования товара меньше 10 символов
     item.name = 'Смартфон'
     assert item.name == 'Смартфон'
@@ -12,10 +11,10 @@ if __name__ == '__main__':
     # Exception: Длина наименования товара превышает 10 символов.
 
     Item.instantiate_from_csv()  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    assert len(Item.data) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    assert item1.name == 'СуперСмарт'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
