2812eac9c270b3b6efd00db9c0d893c9c3ea0d00
Johnny <evgkisevgkis@gmail.com>
2023-08-09 19:31:06 +0500

    Добавлены тесты к магическим методам repr и str

--- tests/test_item.py
+++ tests/test_item.py
@@ -21,3 +21,13 @@ def test_instantiate_from_csv():
 
 def test_string_to_number():
     assert Item.string_to_number('10.3') == 10
+
+
+def test_item_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_item_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
