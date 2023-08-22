c34de2ca5822bca185827d567022325755869132
Johnny <evgkisevgkis@gmail.com>
2023-08-22 19:13:13 +0500

    Доработан класс Phone

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,19 @@ class Phone(Item):
     """Класс для представления телефонов в магазине"""
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self._number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if number_of_sim < 1:
+            raise Exception('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self._number_of_sim = number_of_sim
+            pass
